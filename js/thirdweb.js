5adc83530d8eeaee82619b9df68cb16d74485d92
rabbitflower <rabbitflower.nft@ud.md>
2023-11-15 21:27:36 +0900

    html44

--- js/thirdweb.js
+++
@@ -1,9 +0,0 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-
-const sdk = new ThirdwebSDK("goerli");
-
-// Login with a single line of code
-const payload = await sdk.auth.login();
-
-// And verify the address of the logged in wallet
-const address = await sdk.auth.verify(payload);
